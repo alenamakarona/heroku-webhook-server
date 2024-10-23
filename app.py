a666b3d895620801c113e06f9a5cfdb81cf70955
alenamakarona <iskeevaelena21@gmail.com>
2024-10-23 16:06:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, request, jsonify
+import logging
 import os
 
 app = Flask(__name__)
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    if data is not None and 'transactionStatus' in data:
-        if data['transactionStatus'] == 'Approved':
-            return jsonify({'status': 'success'})
-    return jsonify({'status': 'error'})
+    logging.info(f"Received data: {data}")  # Логирование полученных данных
+
+    if not data:
+        logging.warning("No JSON data received")  # Логирование предупреждения
+        return jsonify({'status': 'error', 'message': 'Invalid JSON'}), 415
+
+    transaction_status = data.get('transactionStatus')
+    logging.info(f"Transaction status: {transaction_status}")  # Логирование статуса транзакции
+
+    if transaction_status == 'Approved':
+        return jsonify({'status': 'success'})
+    
+    return jsonify({'status': 'error'}), 400
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
