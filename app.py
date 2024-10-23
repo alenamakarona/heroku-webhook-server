00b5bf61ee4d16d0afaa1a1c74dda693da9e0969
alenamakarona <iskeevaelena21@gmail.com>
2024-10-23 16:52:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json  # Получение данных из запроса
-    # Обработка данных
+    data = request.json
     print(data)  # Для отладки, чтобы увидеть входящие данные
     return jsonify({"status": "success"}), 200
