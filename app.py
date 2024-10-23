74a820e1e954ed8b2025bf9bf890d32ec4e95050
alenamakarona <iskeevaelena21@gmail.com>
2024-10-23 14:55:08 +0300

    Update app.py
    Update app.py for Heroku deployment

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 def webhook():
     data = request.json
     if data['transactionStatus'] == 'Approved':
-        # Обработка успешной оплаты
         return jsonify({'status': 'success'})
     return jsonify({'status': 'error'})
 
 if __name__ == '__main__':
-    # Указываем, что приложение будет слушать указанный порт
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
